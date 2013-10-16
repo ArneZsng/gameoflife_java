9995115da761b918bda042bcb745c25d732e6131
Hendrik Makait <hendrik.makait@nordakademie.de>
2013-10-16 22:20:05 +0200

    Added 2 !

--- GameOfLife_Semesteraufgabe/src/de/gameoflife/main/MainGame.java
+++ GameOfLife_Semesteraufgabe/src/de/gameoflife/main/MainGame.java
@@ -3,7 +3,7 @@ package de.gameoflife.main;
 public class MainGame {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!!");
 	}
 	
 }
