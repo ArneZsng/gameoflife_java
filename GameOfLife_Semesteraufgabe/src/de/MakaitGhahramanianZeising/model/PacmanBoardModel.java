fc5371068c59be08f1dba6484893c54bcabdfab8
Arne Zeising <arne.zeising@googlemail.com>
2013-10-20 16:11:27 +0200

    Improved Pacman algorithm

--- GameOfLife_Semesteraufgabe/src/de/MakaitGhahramanianZeising/model/PacmanBoardModel.java
+++ GameOfLife_Semesteraufgabe/src/de/MakaitGhahramanianZeising/model/PacmanBoardModel.java
@@ -16,13 +16,16 @@ public class PacmanBoardModel extends AbstractBoardModel {
 	@Override
 	protected int countLivingNeighbors(int x, int y) {
 		int numberOfLivingNeighbors = 0;
-		ArrayList<Integer> adjacentColumns = findAdjacentColumns(x);
-		ArrayList<Integer> adjacentRows = findAdjacentRows(y);
+		x = ensurePositiveCoordinate(x, getWidth());
+		y = ensurePositiveCoordinate(y, getHeight());
 
-		for (int i : adjacentColumns) {
-			for (int j : adjacentRows) {
-				if (!(i == x && j == y)) {
-					if (isAlive(i,j)) {
+		//Test
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (!(i == 0 && j == 0)) {
+					int xCoordinate = ((x+i) % getWidth()); 
+					int yCoordinate = ((y+j) % getHeight());
+					if ( isAlive(xCoordinate,yCoordinate) ) {
 						numberOfLivingNeighbors++;
 					}
 				}
@@ -31,53 +34,12 @@ public class PacmanBoardModel extends AbstractBoardModel {
 
 		return numberOfLivingNeighbors;
 	}
-	
-	private ArrayList<Integer> findAdjacentRows(int y) {
-		ArrayList<Integer> adjacentRows = new ArrayList<Integer>();
-		
-		if (getHeight() == 1) {
-			adjacentRows.add(y);
-		} else {
-			if (y == 0 && getHeight() > 1) {
-				adjacentRows.add(getHeight()-1);
-			} else {
-				adjacentRows.add(y-1);
-			}
-			
-			adjacentRows.add(y);
-			
-			if ((y+1) == getHeight() && getHeight() > 1) {
-				adjacentRows.add(0);
-			} else {
-				adjacentRows.add(y+1);
-			}
-		}
-		
-		return adjacentRows;
-	}
 
-	private ArrayList<Integer> findAdjacentColumns(int x) {
-		ArrayList<Integer> adjacentColumns = new ArrayList<Integer>();
-		
-		if (getWidth() == 1) {
-			adjacentColumns.add(x);
-		} else {
-			if (x == 0 && getWidth() > 1) {
-				adjacentColumns.add(getWidth()-1);
-			} else {
-				adjacentColumns.add(x-1);
-			}
-			
-			adjacentColumns.add(x);
-			
-			if ((x+1) == getWidth() && getWidth() > 1) {
-				adjacentColumns.add(0);
-			} else {
-				adjacentColumns.add(x+1);
-			}
+	private int ensurePositiveCoordinate(int i, int dimension) {
+		if (i == 0) {
+			i += dimension;
 		}
-		
-		return adjacentColumns;
+		return i;
 	}
 
 }
--- GameOfLife_Semesteraufgabe/src/de/MakaitGhahramanianZeising/model/tests/PacmanBoardModelTest.java
+++ GameOfLife_Semesteraufgabe/src/de/MakaitGhahramanianZeising/model/tests/PacmanBoardModelTest.java
@@ -22,15 +22,15 @@ public class PacmanBoardModelTest {
 		cells[1][0] = new CellModel(true);
 		cells[1][1] = new CellModel(true);
 		cells[1][2] = new CellModel(true);
-		cells[2][0] = new CellModel(true);
-		cells[2][1] = new CellModel(true);
+		cells[2][0] = new CellModel(false);
+		cells[2][1] = new CellModel(false);
 		cells[2][2] = new CellModel(true);		
 		board = new PacmanBoardModel(cells);
 		//then
-		assertEquals(8, board.numberOfLivingNeighbors(1,1));
-		assertEquals(8, board.numberOfLivingNeighbors(0,1));
-		assertEquals(8, board.numberOfLivingNeighbors(1,2));
-		assertEquals(8, board.numberOfLivingNeighbors(0,0));
+		assertEquals(6, board.numberOfLivingNeighbors(1,1));
+		assertEquals(6, board.numberOfLivingNeighbors(0,1));
+		assertEquals(6, board.numberOfLivingNeighbors(1,2));
+		assertEquals(6, board.numberOfLivingNeighbors(0,0));
 	}
 	
 	@Test
@@ -39,10 +39,10 @@ public class PacmanBoardModelTest {
 		CellModel[][] cells = new CellModel[1][3];
 		cells[0][0] = new CellModel(true);
 		cells[0][1] = new CellModel(true);
-		cells[0][2] = new CellModel(true);	
+		cells[0][2] = new CellModel(false);	
 		board = new PacmanBoardModel(cells);
 		//then
-		assertEquals(2, board.numberOfLivingNeighbors(0,0));
+		assertEquals(5, board.numberOfLivingNeighbors(0,0));
 	}
 	
 	@Test
@@ -51,10 +51,40 @@ public class PacmanBoardModelTest {
 		CellModel[][] cells = new CellModel[3][1];
 		cells[0][0] = new CellModel(true);
 		cells[1][0] = new CellModel(true);
-		cells[2][0] = new CellModel(true);	
+		cells[2][0] = new CellModel(false);	
 		board = new PacmanBoardModel(cells);
 		//then
-		assertEquals(2, board.numberOfLivingNeighbors(0,0));
+		assertEquals(5, board.numberOfLivingNeighbors(0,0));
+	}
+	
+	@Test
+	public void shouldCountAllLivingNeighborsForDoubleColumn() {
+		//assume
+		CellModel[][] cells = new CellModel[2][3];
+		cells[0][0] = new CellModel(true);
+		cells[0][1] = new CellModel(true);
+		cells[0][2] = new CellModel(false);
+		cells[1][0] = new CellModel(false);
+		cells[1][1] = new CellModel(false);
+		cells[1][2] = new CellModel(true);
+		board = new PacmanBoardModel(cells);
+		//then
+		assertEquals(3, board.numberOfLivingNeighbors(0,0));
+	}
+	
+	@Test
+	public void shouldCountAllLivingNeighborsForDoubleRow() {
+		//assume
+		CellModel[][] cells = new CellModel[3][2];
+		cells[0][0] = new CellModel(true);
+		cells[0][1] = new CellModel(true);
+		cells[1][0] = new CellModel(false);
+		cells[1][1] = new CellModel(true);
+		cells[2][0] = new CellModel(false);
+		cells[2][1] = new CellModel(true);	
+		board = new PacmanBoardModel(cells);
+		//then
+		assertEquals(6, board.numberOfLivingNeighbors(0,0));
 	}
 
 }
