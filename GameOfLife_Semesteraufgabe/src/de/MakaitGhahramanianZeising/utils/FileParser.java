c2850f231d1799ad948dedba6d73d5bc2e408a98
Patrik Ghahramanian <patrik.ghahramanian@nordakademie.de>
2013-10-22 10:47:51 +0200

    work in process

--- GameOfLife_Semesteraufgabe/src/de/MakaitGhahramanianZeising/utils/FileParser.java
+++ GameOfLife_Semesteraufgabe/src/de/MakaitGhahramanianZeising/utils/FileParser.java
@@ -6,52 +6,52 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class FileParser {
-	
-	/** Reads the text file
+
+	static int lengthFirstString;
+	static int x=0;
+	private final Charset ENCODING = StandardCharsets.UTF_16;
+	private final Path fFilePath;
+
+	/** Reads the text file */
 	public static void main(String[] aArgs) throws IOException {
-		AsciParser parser = new AsciParser(
+		FileParser parser = new FileParser(
 				"/Users/patrickghahramanian/Desktop/Java/Asci/MusterText.txt");
 		parser.parseLineByLine();
 	}
-	*/
-	
-	private final Path fFilePath;
-	
+
 	public FileParser(String aFileName) {
 		fFilePath = Paths.get(aFileName);
 	}
 
 	/** Method that reads Line by Line from the text file */
-	private final Charset ENCODING = StandardCharsets.UTF_16;
-
 	public void parseLineByLine() throws IOException {
 		Scanner scanner = new Scanner(fFilePath, ENCODING.name());
+		 lengthFirstString= scanner.next().length();
 		{
 			while (scanner.hasNext()) {
-				parserStringByString(scanner.nextLine());
+				parserLengthLinebyLine(scanner.next());
 			}
 		}
 	}
 
-	/** Method that verifies the lines againts default */
-	private void parserStringByString(String aString) {
-		StringTokenizer tokenizer=new StringTokenizer(aString);
-		while (tokenizer.hasMoreElements()){
-			int lengthToken=tokenizer.nextToken().length();
-			String newString=aString;
-			StringTokenizer newTokenizer= new StringTokenizer(newString);
-			while (newTokenizer.hasMoreElements()){
-				int newLengthToken=newTokenizer.nextToken().length();
-			if(lengthToken!=newLengthToken){
-				output("not valid");
-			}
-			else {
-				output("valid");
-			}
-			}
+	/** Method that sets the length of each Stringtoken from each line */
+	private void parserLengthLinebyLine(String aString) {
+		Scanner scanner = new Scanner(aString);
+		int lengthLine;
+		while (scanner.hasNext()) {
+			lengthLine = scanner.next().length();
+			parserLengthValidation(lengthLine);
+		}
+	}
+	
+	/**Method that checks the file against the length */
+	private boolean parserLengthValidation(int lengthLine) {
+		if (!(lengthLine == lengthFirstString)) {
+			return false;
+		} else {
+			return true;
 		}
 	}
 
